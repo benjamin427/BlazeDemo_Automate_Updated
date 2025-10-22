0438d73a9870a8f5d15586782cc8425b3201c876
Benjamin Saint Elien <shadowboxer79@hotmail.com>
2025-10-22 19:58:31 -0400

    Features, page objects, and support commands

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -24,8 +24,21 @@
 // -- This will overwrite an existing command --
 // Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
 
-Cypress.Commands.add('login_url', () => {
+Cypress.Commands.add("login_url", () => {
     cy.visit("https://blazedemo.com/login")
+    cy.get('a').invoke('attr', "href").then(href => {
+        cy.request(href).its('status').should('eq', 200)
+    })
+})
+Cypress.Commands.add("register_url", () => {
+    cy.visit("https://blazedemo.com/register")
+    cy.get('a').invoke("attr", "href").then(href => {
+        cy.request(href).its('status').should('eq', 200)
+    })
+
+})
+Cypress.Commands.add('resetPassword_url', () => {
+    cy.visit("https://blazedemo.com/password/reset")
     cy.get('a').invoke("attr", "href").then(href => {
         cy.request(href).its('status').should("eq", 200)
     })
